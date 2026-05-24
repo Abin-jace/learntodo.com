52ee94db6857bccf6a86744016599752d38a5c7e
Abin-jace <bajv312@gmail.com>
2026-05-24 16:45:59 +0530

    Updated homepage UI with glassmorphism and icons

--- supabase.js
+++ supabase.js
@@ -1,11 +1,4 @@
-const supabaseUrl =
-  "https://dxaqybpfdsuzdrscxtam.supabase.co"
+const SUPABASE_URL = "https://dxaqybpfdsuzdrscxtam.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4YXF5YnBmZHN1emRyc2N4dGFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NDE0NTQsImV4cCI6MjA5NTExNzQ1NH0.w88qliGPGh63nFYrMxCEAv6MNW7aOZlRmSTfReazD_4"
 
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4YXF5YnBmZHN1emRyc2N4dGFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NDE0NTQsImV4cCI6MjA5NTExNzQ1NH0.w88qliGPGh63nFYrMxCEAv6MNW7aOZlRmSTfReazD_4"
-
-const supabaseClient =
-  supabase.createClient(
-    supabaseUrl,
-    supabaseKey
-  )
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
